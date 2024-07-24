c0498bccae46ac854746b94847d09c5e955c411a
weirdinside <ani@weirdinsi.de>
2024-07-24 02:05:36 -0400

    sprint 13 commit 7

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const cors = require('cors');
 const mongoose = require("mongoose");
 const router = require("./routes/index");
-const cors = require('cors');
 
 const app = express();
 const { PORT = 3001 } = process.env;
